fe212c9552d1d0d2afb7fd6a7b1d9b797211ab38
Shidi Yang <nicebling@sina.com>
2019-01-26 09:51:31 +0800

    new file:   BitManipulation/APlusBProblem.java 	new file:   BitManipulation/CountingBits.java 	new file:   Matrix/SparseMatrixMultiplication.java

---
+++ BitManipulation/APlusBProblem.java
@@ -0,0 +1,21 @@
+package BitManipulation;
+/*Write a function that add two numbers A and B.
+ * You should not use + or any arithmetic operators.*/
+public class APlusBProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b 
+     */
+    public int aplusb(int a, int b) {
+		return b;
+        // write your code here
+    }
+
+}
