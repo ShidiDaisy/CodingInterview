fe59ebb44a7adfdd586796cc96130f7a2e7abae6
ShidiDaisy <nicebling@sina.com>
2019-02-18 23:10:56 +0800

    modified:   Math/MaxSubarray.java

--- Math/MaxSubarray.java
+++ Math/MaxSubarray.java
@@ -10,21 +10,24 @@ public class MaxSubarray {
 
 	public static void main(String[] args) {
 		int[] A = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-		System.out.println(kadane(A));
+		int[] B = {-8, -3, -6, -2, -5, -4};
+		System.out.println(kadane(B));
 	}
 	
 	public static int kadane(int[] A) {
 		//stores maximum sum sub-array found so far
-		int maxEndingHere = 0;
-		int max = 0;
+		int curMax = A[0];
+		int max = A[0];
 		
-		for(int a:A) {
-			maxEndingHere = maxEndingHere + a;
+		for(int i=1; i<A.length; i++) {
+			curMax = curMax + A[i];
 			
-			//Not counted if the number is less than 0
-			maxEndingHere = Integer.max(maxEndingHere, 0);
+			//if this num is making the sum smaller, then should get rid of this num.
+			//If (sum + thisNum) < thisNum -> sum<0 && sum<thisNum -> get rid of this sum, start counting
+			//a new sum with thisNum
+			curMax = Integer.max(curMax, A[i]);
 			
-			max = Integer.max(max, maxEndingHere);
+			max = Integer.max(max, curMax);
 		}
 		
 		return max;
